795677237afbe425db3bec3fa09b967448651f16
André Sampaio <andresampaio@id.uff.br>
2023-04-28 16:02:12 -0300

    Post new users

--- UsuariosApi/Program.cs
+++ UsuariosApi/Program.cs
@@ -24,6 +24,8 @@ namespace UsuariosApi
                 .AddEntityFrameworkStores<UsuarioDbContext>()
                 .AddDefaultTokenProviders();
 
+            builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
